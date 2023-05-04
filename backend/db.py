4112fc874ac9ce8e1ff7f7939ca1669ed6d78985
Prakriti <pt293@cornell.edu>
2023-05-04 19:51:33 -0400

    Implemented authentication

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy 
+import datetime 
+import hashlib 
+import os 
+import bcrypt 
+
 
 db = SQLAlchemy()
 
@@ -8,11 +13,19 @@ class User(db.Model):
     """
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=True)
     bank_balance = db.Column(db.Integer, nullable=True)
-    dark = db.Column(db.Boolean, nullable=False)
-    quiet = db.Column(db.Boolean, nullable=False)
-    region = db.Column(db.String, nullable=False)
+    dark = db.Column(db.Boolean, nullable=True)
+    quiet = db.Column(db.Boolean, nullable=True)
+    region = db.Column(db.String, nullable=True)
+    # User information
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_digest = db.Column(db.String, nullable=False)
+
+    # Session information
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
 
     def __init__(self, **kwargs):
         """
@@ -23,6 +36,9 @@ class User(db.Model):
         self.dark = kwargs.get("dark", False)
         self.quiet = kwargs.get("quiet", False)
         self.region = kwargs.get("region","")
+        self.email = kwargs.get("email")
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
 
 
     def serialize(self):
@@ -37,6 +53,42 @@ class User(db.Model):
             "quiet":self.quiet, 
             "region" : self.region
         }
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session to be a day from now
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        """
+        Verifies the password of a user
+        """
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        """
+        Verifies the session token of a user
+        """
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
+
 
 class Location(db.Model):
     """
